2481949a888cb02d51d6ed1384feb6c0b9bb95e1
oldriverdev-prog <oldriverdev@gmail.com>
2025-08-10 13:21:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const ytdl = require('@distube/ytdl-core');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Archivos estáticos (frontend)
 app.use(express.static(path.join(__dirname, 'public')));
